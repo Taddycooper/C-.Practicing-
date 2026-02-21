bae00579191503a5abf2dfd7506fe326756b6c53
taddycooper <Nathaniel.sweany@gmail.com>
2026-02-20 21:48:16 -0500

    Create randomNumber.cpp

---
+++ randomNumber.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int secret = rand() % 100 + 1;
+    int guess;
+
+    cout << "Guess the number 1-100" << endl;
+
+    do {
+        cout << "Enter guess: ";
+        cin >> guess;
+
+        if (guess > secret) {
+            cout << "Too high!" << endl;
+        }
+        else if (guess < secret) {
+            cout << "Too low" << endl;
+        }
+        else {
+            cout << "Correct" << endl;
+        }
+
+    } while (guess != secret);
+
+    return 0;
+}
